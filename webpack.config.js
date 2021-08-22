39a94314ae11329fae6d1ec71c6d32690e2b5aad
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-22 19:51:27 -0400

    basic react frontend is setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,28 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
   }
 }
 module.exports = config;
