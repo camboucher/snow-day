4bfbc4f20060be9279494ea6f9292500cb7c2826
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-23 01:52:48 -0400

    testing out geolocation api and added helperfunc for calculating distance from lat/long coords

--- server/index.js
+++ server/index.js
@@ -4,8 +4,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'../dist')));
+
+// app.get('/test', (res, req) => {
+//   res.send(navigator.geolocation.getCurrentPosition((data) => {
+//     console.log(data)
+//   }, (err) => {
+//     console.log(err)
+//   }))
+// })
+
 app.listen(port, () => {
   console.log('Server is listening on port 3000');
 });
-
-app.use(express.static(path.join(__dirname,'../dist')));
--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-        <button type="button" class="btn btn-primary">
-          This is a bootstrap button
-        </button>
-      </>
-    );
+function App(props) {
+
+  const [location, setLocation] = useState([0,0]);
+
+  const locationSuccess = (position) => {
+    setLocation[position.coords.latitude, position.coords.longitude]
   }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(locationSuccess, (err) => {
+      console.log(`Unable to locate user's location`)
+    });
+  });
+
+  return (
+    <>
+      <h1>
+        Hello
+      </h1>
+      <button type="button" class="btn btn-primary">
+        This is a bootstrap button
+      </button>
+    </>
+  );
 }
 
 export default App;
