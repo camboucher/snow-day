8f1630ea2a767a52716b358899f40125c0ac0cf7
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-25 09:21:07 -0400

    fixed rounding temps

--- src/Components/WeatherForecast.js
+++ src/Components/WeatherForecast.js
@@ -11,12 +11,12 @@ function WeatherForecast(props) {
         <div class="col-md forecast-main">
           <p id="forecast-day-name">{days[(new Date().getDay() + forecastDay + 7) % 7]}</p>
           <div class="row">
-            <h6 id="forecast-day-main">{weatherData.temp.day}°</h6>
+            <h6 id="forecast-day-main">{Math.round(weatherData.temp.day)}°</h6>
             {/* <i class="wi forecast-icon" id="forecast-day-1-icon"><img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}></img></i> */}
           </div>
         </div>
         <div class="row">
-            <p><span class="prob-precipitation" id="forecast-day-1-ht">Precip: {weatherData.pop * 100}%
+            <p><span class="prob-precipitation" id="forecast-day-1-ht">Precip: {Math.round(weatherData.pop * 100)}%
              {weatherData.snow ? ' ' + Math.round(weatherData.snow) :
                 weatherData.rain ? ' ' + Math.round(weatherData.rain) : 0} in.</span></p>
         </div>
