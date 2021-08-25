414287e000f9e924dcdcb3b14ab94d39c6500fd1
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-25 07:47:29 -0400

    made public and cloning down config for tokens

---
+++ config.js
@@ -0,0 +1,8 @@
+const TOKENS = {
+  googleMapsKey: 'AIzaSyB9OhQgdTRJ3S5XRO9HrFAyI5o-xLCmkaw',
+  openWeatherToken: '6ecec238071e6d0ca0148e6c11c8f47c'
+};
+
+const url = 'http://localhost';
+
+export { url, TOKENS };
