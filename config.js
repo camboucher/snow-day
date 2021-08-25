3099f53215f636d0a68758ef68d0a1880d1330dc
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-25 07:56:56 -0400

    updated server route config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const TOKENS = {
   openWeatherToken: '6ecec238071e6d0ca0148e6c11c8f47c'
 };
 
-const url = 'http://localhost';
+const url = 'ec2-3-134-86-209.us-east-2.compute.amazonaws.com';
 
 export { url, TOKENS };
