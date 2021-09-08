5190e8f9fea5e11d4ee4d700836cc42a6e52a4b7
Cam Boucher <32146990+camboucher@users.noreply.github.com>
2021-09-08 08:44:30 -0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const TOKENS = {
-  googleMapsKey: 'AIzaSyB9OhQgdTRJ3S5XRO9HrFAyI5o-xLCmkaw',
-  openWeatherToken: '6ecec238071e6d0ca0148e6c11c8f47c'
-};
-
-const url = '3.129.71.42';
-
-export { url, TOKENS };
